9e3298e3adf5d61dbf0464d3d7c33e0bc27b8478
Wagner Silva <wagnergreen21@gmail.com>
2021-01-25 21:17:00 -0300

    redux

--- apps/brewsket/src/App.tsx
+++ apps/brewsket/src/App.tsx
@@ -1,8 +1,14 @@
 import React from 'react';
+import { Provider } from 'react-redux';
 
 import Router from './navigation';
+import { store } from './store';
 
-const App: React.FC = () => <Router />;
+const App: React.FC = () => (
+  <Provider store={store}>
+    <Router />
+  </Provider>
+);
 
 // @ts-ignore
 if (window.Cypress) {
